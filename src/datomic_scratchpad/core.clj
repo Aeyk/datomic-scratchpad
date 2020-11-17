da20c374e69d3469b2bb2f70a599ac6cdab65aad
Malik Kennedy <mksybr@gmail.com>
2020-11-17 15:10:00 -0800

    Creating a database

--- src/datomic_scratchpad/core.clj
+++ src/datomic_scratchpad/core.clj
@@ -1,8 +1,9 @@
 (ns datomic-scratchpad.core
   (:require [datomic.dev-local :as dl]
             [datomic.client.api :as d]))
-
 (def client (d/client {:server-type :dev-local
                        :system "datomic-samples"}))
+(d/create-database client {:db-name "hello-world"})
+(def conn (d/connect client {:db-name "hello-world"}))
+(def db (d/db conn))
 
-(d/list-databases client {})
